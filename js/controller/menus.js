3daf10b3437820a85cf203217b99caa0ec50f3d6
Antoine Brossault <contact@antoinebrossault.com>
2016-04-14 22:01:53 +0200

    Menu responsive ctrl

--- js/controller/menus.js
+++ js/controller/menus.js
@@ -7,11 +7,38 @@ app.controller('MenusCtrl', function($scope,Menu){
 
     $scope.menu = datas.items;
 
-
   },function(msg){
     alert(msg);
   });
 
+
+
+  /**
+  *
+  *  SHOW MENU
+  *
+  */
+  $scope.showMenu = function(){
+    console.log("menu Open");
+    $("#responsive-icon").addClass('hidden');
+    $('#responsive-icon-close').removeClass('hidden');
+    $('.slide-menu nav').addClass('responsive-menu-show');
+  };
+
+
+
+  /**
+  *
+  *  HIDE MENU
+  *
+  */
+  $scope.hideMenu = function(){
+    console.log("menu Close");
+    $("#responsive-icon-close").addClass('hidden');
+    $('#responsive-icon').removeClass('hidden');
+    $('.slide-menu nav').removeClass('responsive-menu-show');
+
+  };
 
 
 
